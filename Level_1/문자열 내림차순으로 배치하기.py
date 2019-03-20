2b58b177c950984ff8b3172c85afa3e18306cf8a
naayoung <ask5046a@gmail.com>
2019-03-21 01:44:00 +0900

    level1

---
+++ Level_1/문자열 내림차순으로 배치하기.py
@@ -0,0 +1,15 @@
+# 문자열 s에 나타나는 문자를 큰것부터 작은 순으로 정렬해 새로운 문자열을 리턴하는 함수, solution을 완성해주세요.
+# s는 영문 대소문자로만 구성되어 있으며, 대문자는 소문자보다 작은 것으로 간주합니다.
+
+
+def solution(s):
+    answer = list(s)
+    answer.sort(reverse=True)
+    answer = "".join(answer)
+    return answer
+
+
+# 참고 풀이
+def solution(s):
+    answer = ''.join(sorted(s, reverse=True))
+    return answer
